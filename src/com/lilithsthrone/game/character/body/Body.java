1d5f8e4842f1e76942c2c15e557b7527cbd1d8a3
CravenAnchorite <34547860+CravenAnchorite@users.noreply.github.com>
2017-12-24 20:05:13 -0500

    pre rebase

--- src/com/lilithsthrone/game/character/body/Body.java
+++ src/com/lilithsthrone/game/character/body/Body.java
@@ -1684,8 +1684,6 @@ public class Body implements Serializable, XMLSaving {
 			bodyCore.appendChild(element);
 			
 			CharacterUtils.addAttribute(doc, element, "type", bct.toString());
-			System.out.println(bct.toString());
-			if (this.coverings.get(bct) != null) {
 				CharacterUtils.addAttribute(doc, element, "pattern", this.coverings.get(bct).getPattern().toString());
 				CharacterUtils.addAttribute(doc, element, "colourPrimary", this.coverings.get(bct).getPrimaryColour().toString());
 				CharacterUtils.addAttribute(doc, element, "glowPrimary", String.valueOf(this.coverings.get(bct).isPrimaryGlowing()));
@@ -1693,7 +1691,6 @@ public class Body implements Serializable, XMLSaving {
 				CharacterUtils.addAttribute(doc, element, "glowSecondary", String.valueOf(this.coverings.get(bct).isSecondaryGlowing()));
 				CharacterUtils.addAttribute(doc, element, "discovered", String.valueOf(this.getBodyCoveringTypesDiscovered().contains(bct)));
 			}
-		}
 		
 
 		// Antennae:
